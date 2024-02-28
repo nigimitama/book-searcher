678e14751af66152e75e5b767ae31a27aec50134
nigimitama <nigimitama16@gmail.com>
2024-02-28 21:33:15 +0900

    feature: 書籍のサムネを表示

--- src/components/BookSummary.tsx
+++ src/components/BookSummary.tsx
@@ -1,3 +1,4 @@
+import { Stack } from '@mui/material'
 import { Item } from '../@types/SearchResult'
 import Link from '@mui/material/Link'
 import ListItem from '@mui/material/ListItem'
@@ -11,24 +12,30 @@ export const BookSummary = (item: Item): JSX.Element => {
 
   return (
     <ListItem>
-      <ListItemText
-        primary={
-          <Link
-            href={item.volumeInfo.previewLink}
-            target="_blank"
-            rel="noreferrer"
-            sx={{ fontSize: "14px" }}
-          >
-            {item.volumeInfo.title}
-          </Link>
-        }
-        secondary={
-          <>
-            {authors} - {item.volumeInfo.publisher} {publishedYear}
-          </>
-        }
-        secondaryTypographyProps={{ sx: { fontSize: "12px" } }}
-      />
+      <Stack direction="row" spacing={2}>
+        <img
+          src={item.volumeInfo.imageLinks.smallThumbnail}
+          style={{ width: "50px" }}
+        />
+        <ListItemText
+          primary={
+            <Link
+              href={item.volumeInfo.previewLink}
+              target="_blank"
+              rel="noreferrer"
+              sx={{ fontSize: "14px" }}
+            >
+              {item.volumeInfo.title}
+            </Link>
+          }
+          secondary={
+            <>
+              {authors} - {item.volumeInfo.publisher} {publishedYear}
+            </>
+          }
+          secondaryTypographyProps={{ sx: { fontSize: "12px" } }}
+        />
+      </Stack>
     </ListItem>
   )
 }
