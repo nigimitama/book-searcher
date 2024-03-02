3c9be953152468fba593d14aaf77aeac567ab8e4
nigimitama <nigimitama16@gmail.com>
2024-03-02 10:58:11 +0900

    feature: Headerのデザインを変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 
 
 export const Header: React.FC = () => {
   return (
     <AppBar
       position="static"
-      sx={{ backgroundColor: "dimgray" }}
     >
       <Toolbar variant="dense">
-        <Typography variant="h6" color="inherit" component="div">
+        <span style={{ fontSize: 18, fontWeight: 600 }}>
           Book Searcher
-        </Typography>
+        </span>
       </Toolbar>
     </AppBar>
   )
